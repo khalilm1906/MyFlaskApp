ec4d4316bdea6eb2c3fb61881f95bb16becd79c4
Khalil Malik <Khalilm1906@gmail.com>
2017-12-02 21:08:24 -0800

    initial checkin

---
+++ src/data.py
@@ -0,0 +1,37 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur \
+                    adipisicing elit. Atque commodi dignissimos \
+                    dolore eaque natus optio, quae quod recusandae \
+                    suscipit tenetur veritatis voluptate. Aspernatur \
+                    at facilis mollitia sapiente sunt tempore totam.',
+            'author': 'Khalil Malik',
+            'create_date': '12-02-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur \
+                    adipisicing elit. Atque commodi dignissimos \
+                    dolore eaque natus optio, quae quod recusandae \
+                    suscipit tenetur veritatis voluptate. Aspernatur \
+                    at facilis mollitia sapiente sunt tempore totam.',
+            'author': 'Jone Doe',
+            'create_date': '11-13-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur \
+                    adipisicing elit. Atque commodi dignissimos \
+                    dolore eaque natus optio, quae quod recusandae \
+                    suscipit tenetur veritatis voluptate. Aspernatur \
+                    at facilis mollitia sapiente sunt tempore totam.',
+            'author': 'Jane Doe',
+            'create_date': '10-31-2017'
+        }
+    ]
+    return articles
